416cfbd4141162680efe381165f76c63e82e34d7
Joe Clark <weirdlookingjay@live.com>
2025-04-14 12:12:02 -0400

    Set up Django REST Framework backend with initial apps and configuration

---
+++ backend/core/asgi.py
@@ -0,0 +1,19 @@
+"""
+ASGI config for core project.
+"""
+
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            # WebSocket URLs will be added here
+        ])
+    ),
+})
